a9f3437df646bdd611863aa4c5954df83221175a
Nikitin Egor <EgorND1998@gmail.com>
2022-03-10 10:44:24 +0900

    commit

--- дз2.py
+++ дз2.py
@@ -9,16 +9,16 @@ with open(filename, "r", encoding="utf-8") as f:
 print(contacts_list)
 
 new_list1 = list()
-for i in contacts_list:
-    row = ",".join(i)
+for lists in contacts_list:
+    row = ",".join(lists)
     pattern = r"(\+?)(\d{1})(\s?)(\(?)(\d{3})(\)?)(.?)(\d{3})(.?)(\d{2})(.?)(\d{2})(\s?)(\(?)(\доб.)?(\s?)(\d{4})?(\)?)"
     sub = r"+7(\5)\8-\10-\12 \15\17"
     result = re.sub(pattern, sub, row)
     new_list1.append(result.split(","))
 
 new_list2 = list()
-for i in new_list1:
-    row = ",".join(i)
+for lists in new_list1:
+    row = ",".join(lists)
     pattern = r"([А-Я])([а-я]+)(.)([А-Я])([а-я]+)(.)(\,)?(\,)?(\,)?([А-Я])?([а-я]+)?(\W)+"
     sub = r"\1\2,\4\5,\10\11,\7\8\9"
     result = re.sub(pattern, sub, row)
@@ -26,11 +26,11 @@ for i in new_list1:
 
 new_list3 = list()
 new_list4 = list()
-for id, i in enumerate(new_list2):
-    if i[0] not in new_list3:
-        new_list3.append(i[0])
+for id, lists in enumerate(new_list2):
+    if lists[0] not in new_list3:
+        new_list3.append(lists[0])
     else:
-        new_list4.append(i[0])
+        new_list4.append(lists[0])
 
 set1 = set(new_list3)
 set2 = set(new_list4)
@@ -39,13 +39,13 @@ new_list5 = list(set3)
 
 new_list6 = list()
 new_list7 = list()
-for i in new_list2:
-    if i[0] not in new_list5:
-        new_list6.append(i)
+for lists in new_list2:
+    if lists[0] not in new_list5:
+        new_list6.append(lists)
     else:
-        new_list7.append(i)
+        new_list7.append(lists)
 
-for id, i in enumerate(new_list6):
+for id, lists in enumerate(new_list6):
     pass
 
 constant = 0
@@ -68,10 +68,16 @@ while constant1 <= (id):
         constant += 1
         constant1 += 1
 
-for i in new_list8:
-    new_list7.append(i)
+for lists in new_list8:
+    new_list7.append(lists)
 
 filename = "new_phonebook_raw.csv"
 with open(filename, "w", encoding="utf-8", newline="") as f:
   writer = csv.writer(f, delimiter=",")
   writer.writerows(new_list7)
+
+with open(filename, "r", encoding="utf-8") as f:
+  rows = csv.reader(f)
+  contacts_list = list(rows)
+print(contacts_list)
+
